88711dfc0ce562bb3ecd93e4582839d053c47011
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-07 11:16:33 +0300

    .

---
+++ src/main/java/com/tugalsan/blg/semaphore/Main.java
@@ -0,0 +1,33 @@
+package com.tugalsan.blg.semaphore;
+
+import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.thread.server.async.TS_ThreadAsyncAwait;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import java.time.Duration;
+import java.util.concurrent.Semaphore;
+
+//https://www.javatpoint.com/java-semaphore
+public class Main {
+
+    private static final TS_Log d = TS_Log.of(Main.class);
+
+    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.semaphore
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
+    //java -jar target/com.tugalsan.tst.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
+    public static void main(String... s) {
+        TGS_UnSafe.run(() -> {
+            var threadKiller = TS_ThreadSyncTrigger.of();//not used
+            var threadLimitor = new Semaphore(2);
+            var threadUntil = Duration.ofMinutes(5);//AN EXXECCESSIVE AMOUNT
+            var await = TS_ThreadAsyncAwait.callParallel(threadKiller, threadUntil,
+                    Runner.of("#1", threadLimitor),//Runner.of("incrementor0", Runner.TYPE.INCREMENTOR),
+                    Runner.of("#2", threadLimitor),//Runner.of("incrementor1", Runner.TYPE.INCREMENTOR),
+                    Runner.of("#3", threadLimitor),//Runner.of("decrementor0", Runner.TYPE.DECREMENTOR),
+                    Runner.of("#4", threadLimitor)//Runner.of("decrementor1", Runner.TYPE.DECREMENTOR)
+            );
+            d.cr("main", "await.hasError: %b".formatted(await.hasError()));
+//            d.cr("main", "count: %d".formatted(Common.COUNT));
+        }, e -> d.ce("main", e));
+    }
+}
--- src/main/java/com/tugalsan/blg/semaphore/Runner.java
+++ src/main/java/com/tugalsan/blg/semaphore/Runner.java
@@ -1,15 +1,17 @@
-package com.tugalsan.blgsemaphore;
+package com.tugalsan.blg.semaphore;
 
 import com.tugalsan.api.callable.client.TGS_CallableType1;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.thread.server.TS_ThreadWait;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import java.time.Duration;
+import java.util.concurrent.Semaphore;
 import java.util.stream.IntStream;
 
-public class ThreadRunner implements TGS_CallableType1<Void, TS_ThreadSyncTrigger> {
+public class Runner implements TGS_CallableType1<Void, TS_ThreadSyncTrigger> {
 
-    private static final TS_Log d = TS_Log.of(ThreadRunner.class);
+    private static final TS_Log d = TS_Log.of(Runner.class);
 
     @Override
     public Void call(TS_ThreadSyncTrigger threadKiller) {
@@ -19,47 +21,39 @@ public class ThreadRunner implements TGS_CallableType1<Void, TS_ThreadSyncTrigge
         TGS_UnSafe.run(() -> {
             d.cr("run", "Starting thread %s".formatted(threadName));
             d.cr("run", "%s is waiting for a permit.".formatted(threadName));
-            Common.SEMAPHORE.acquire();
+            threadLimitor.acquire();
             d.cr("run", "%s gets a permit.".formatted(threadName));
-            act(threadKiller, type);
+            act(threadKiller/*, type*/);
         }, e -> {
             d.ct("run.%s".formatted(threadName), e);
         }, () -> {
             d.cr("run", "Thread %s releases the permit.".formatted(threadName));
-            Common.SEMAPHORE.release();
+            threadLimitor.release();
         });
         return null;
     }
 
-    public static enum TYPE {
-        INCREMENTOR, DECREMENTOR
-    };
-
-    private ThreadRunner(String threadName, TYPE type) {
+    private Runner(String threadName, Semaphore threadLimitor) {
         this.threadName = threadName;
-        this.type = type;
+        this.threadLimitor = threadLimitor;
     }
     final private String threadName;
-    final private TYPE type;
+    final private Semaphore threadLimitor;
 
-    public static ThreadRunner of(String threadName, TYPE type) {
-        return new ThreadRunner(threadName, type);
+    public static Runner of(String threadName, Semaphore threadLimitor) {
+        return new Runner(threadName, threadLimitor);
     }
 
-    private void act(TS_ThreadSyncTrigger threadKiller, TYPE type) {
+    private void act(TS_ThreadSyncTrigger threadKiller/*, TYPE type*/) {
         IntStream.range(0, 5).sequential().forEach(i -> {
             if (threadKiller.hasTriggered()) {
                 return;
             }
             TGS_UnSafe.run(() -> {
-                if (type == TYPE.INCREMENTOR) {
-                    Common.COUNT++;
-                } else if (type == TYPE.DECREMENTOR) {
-                    Common.COUNT--;
-                }
-                d.cr("act", "%s: %d".formatted(threadName, Common.COUNT));
-                TS_ThreadWait.of(threadName, Common.THEAD_KILLER, Common.WORK_LOAD);
+                d.cr("act", "%s: running...".formatted(threadName));
+                TS_ThreadWait.of(threadName, threadKiller, WORK_LOAD);
             });
         });
     }
+    final private static Duration WORK_LOAD = Duration.ofSeconds(1);//A SMALL AMOUNT
 }
