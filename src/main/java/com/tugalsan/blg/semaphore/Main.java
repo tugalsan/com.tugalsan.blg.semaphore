3ab44e1f97558affbdc43b585685724f0060b931
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-07 15:02:09 +0300

    Update Main.java

--- src/main/java/com/tugalsan/blg/semaphore/Main.java
+++ src/main/java/com/tugalsan/blg/semaphore/Main.java
@@ -7,7 +7,6 @@ import com.tugalsan.api.thread.server.async.TS_ThreadAsyncAwait;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.time.Duration;
-import java.util.concurrent.Semaphore;
 import java.util.stream.IntStream;
 
 //https://www.javatpoint.com/java-semaphore
@@ -21,13 +20,11 @@ public class Main {
     public static void main(String... s) {
         TGS_UnSafe.run(() -> {
             var threadKiller = TS_ThreadSyncTrigger.of();//not used on this project
-            var threadLimitor = new Semaphore(2);
+            var threadRateLimit = 2;
             var threadUntil = Duration.ofMinutes(5);//AN EXXECCESSIVE AMOUNT
-            var await = TS_ThreadAsyncAwait.callParallel(threadKiller, threadLimitor, threadUntil,
-                    Caller.of("#1"),
-                    Caller.of("#2"),
-                    Caller.of("#3"),
-                    Caller.of("#4")
+            var await = TS_ThreadAsyncAwait.callParallelRateLimited(threadKiller, threadRateLimit, threadUntil,
+                    Caller.of("#1"), Caller.of("#2"),
+                    Caller.of("#3"), Caller.of("#4")
             );
             d.cr("main", "await.hasError: %b".formatted(await.hasError()));
         }, e -> d.ce("main", e));
