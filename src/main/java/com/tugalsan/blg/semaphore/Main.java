37f1ea119cdce897fce7b5f184af565b54ffaef6
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-07 11:47:53 +0300

    Update Main.java

--- src/main/java/com/tugalsan/blg/semaphore/Main.java
+++ src/main/java/com/tugalsan/blg/semaphore/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
     private static final TS_Log d = TS_Log.of(Main.class);
 
-    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.semaphore
-    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
-    //java -jar target/com.tugalsan.tst.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
+    //cd C:\me\codes\com.tugalsan\blg\com.tugalsan.blg.semaphore
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.blg.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
+    //java -jar target/com.tugalsan.blg.semaphore-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
         TGS_UnSafe.run(() -> {
             var threadKiller = TS_ThreadSyncTrigger.of();//not used on this project
